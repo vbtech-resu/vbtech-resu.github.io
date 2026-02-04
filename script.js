53df59252d66d37abcc8766f6179cfe743191ef2
vbtech-resu <sahuvb16298@gmail.com>
2026-02-04 13:12:47 +0530

    script.js

--- script.js
+++ script.js
@@ -85,17 +85,4 @@ function downloadResume(){
   }).from(resume).save();
   }
 }
-
-
-html2pdf().set({
-  margin: 10,
-  filename: "My_Resume.pdf",
-  image: { type: "jpeg", quality: 0.98 },
-  html2canvas: { scale: 2 },
-  jsPDF: {
-    unit: "mm",
-    format: "a4",
-    orientation: "portrait"
-  },
-  pagebreak: { mode: ["avoid-all"] }
-}).from(resume).save();
+;
